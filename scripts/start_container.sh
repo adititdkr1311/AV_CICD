21a2a342eeb296c506f8c56544c64ed99ac6a6d6
AditiT <adititdkr1311@gmail.com>
2024-11-26 13:18:42 +0530

    sample

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull adititdkr13/adititdkr-devops:2
 
-# Run the Docker image as a container
+# Run the Docker image as a containers
 docker run -d -p 5000:5000 adititdkr13/adititdkr-devops:2
